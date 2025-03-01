f6f574bb69f80cfabc57ddbf43aba9183eb600dd
TrinhDuongHoan <trinhduonghoan2004@gmail.com>
2025-03-02 01:02:55 +0700

    Thêm chức năng vô hiệu hóa user, đặt lại mật khẩu cho user

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from app.models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+from .models import UserProfile
 
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_active')  
+    list_filter = ('is_active',)  
+    search_fields = ('username', 'email')
+
+    actions = ['disable_users', 'enable_users']
+
+    # Action để disable user
+    def disable_users(self, request, queryset):
+        queryset.update(is_active=False)
+        self.message_user(request, "Đã vô hiệu hóa người dùng thành công.")
+    disable_users.short_description = "Disable các user đã chọn"
+
+    # Action để enable user
+    def enable_users(self, request, queryset):
+        queryset.update(is_active=True)
+        self.message_user(request, "Đã kích hoạt lại người dùng thành công.")
+    enable_users.short_description = "Enable các user đã chọn"
+
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+
+    
+
+
+
+
+
--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -7,3 +10,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_disabled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+    
